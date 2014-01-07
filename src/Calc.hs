1fc5085a53b661bdad23e22ab8b3c6ec8a4ccc04
Augustin Juričić <augustin.juricic@gmail.com>
2014-01-07 14:15:45 +0000

    added Token datatype declaration

--- src/Calc.hs
+++ src/Calc.hs
@@ -1,13 +1,19 @@
 module Calc where
 
-data Operator = Plus | Minus | Div | Times
+data Operator = Plus | Minus | Div | Times deriving (Show, Eq)
 opToChar :: Operator -> Char
 opToChar Plus = '+'
 opToChar Minus = '-'
 opToChar Div = '/'
 opToChar Times = '*'
 
-data Token
+data Token = TokOp Operator | TokIdent String | TokNum Integer deriving (Show, Eq)
+
+showContent :: Token -> String
+showContent (TokOp op) = show $ opToChar op
+showContent (TokIdent str) = str
+showContent (TokNum i) = show i
+
 data Expression
 
 tokenize :: String -> [Token]
@@ -23,7 +29,7 @@ main :: IO ()
 main = do
     line <- getLine
     putStrLn line
-    if length line > 0 then
+    if length line /= 0 then
         main
     else
         return ()
