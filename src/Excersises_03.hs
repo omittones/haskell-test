4d36c4dd3b5de84ff8f1b9d8ca7a78ea3141ec95
Augustin Juričić <augustin.juricic@gmail.com>
2014-01-24 22:56:01 +0000

    PI calculation algorithm

--- src/Excersises_03.hs
+++ src/Excersises_03.hs
@@ -1,6 +1,24 @@
 module Excersises_03 where
 
+
+calcpi = last $ take 2000000 sums
+sums = scanl (\x (y,s) -> x + (s*y*4)) 0 order'
+order' = zip order (cycle [1,(-1)])
+order = map (\x -> 1.0 / x) [1,3..]
+
+--total = 0
+--state = 1
+--for i in range(1,1000000,2):
+--    total+= (1.0/i ) * state
+--    state*=-1
+--print total*4
+
 main = do
+
+    print $ calcpi
+    return ()
+
+main10 = do
 
     --getLine returns IO String, <- executes it and converts it to String
     putStrLn "Hello, what's your name?"
